d1a86a38091e7eff7ef7d90c4f5d1f07b439e8ee
Eugene Garay <Yauhen.garay@gmail.com>
2022-11-28 15:20:15 +0300

    New commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import axios from "axios";
 
 export default function Home() {
+
+  const getData = () => {
+    axios.get("https://api.ipify.org?format=json").then(({ data }) => {
+      console.log(data)
+    }).catch((error) => {
+      console.error(error)
+    })
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +25,7 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <button onClick={getData}>getData</button>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
